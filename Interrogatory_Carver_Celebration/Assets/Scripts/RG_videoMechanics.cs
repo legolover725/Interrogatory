1617d0ecc49e91ca45fd94341a1670c90e4a5b56
legolover725 <79878469+legolover725@users.noreply.github.com>
2024-02-23 21:50:11 -0500

    Additional commenting on RG_videoMechanics script

--- Interrogatory_Carver_Celebration/Assets/Scripts/RG_videoMechanics.cs
+++ Interrogatory_Carver_Celebration/Assets/Scripts/RG_videoMechanics.cs
@@ -14,8 +14,10 @@ public class RG_videoMechanics : MonoBehaviour
     void Start()
     {
         vP = GetComponent<VideoPlayer>();
+        //creates new colors for white (no tint on image) and transparent (image isn't visible)
         white = new Color(1f, 1f, 1f, 1f);
         transparent = new Color(1f, 1f, 1f, 0f);
+        //sets play button to invisible
         GameObject.Find("Play").GetComponent<Image>().color = transparent;
     }
 
@@ -25,16 +27,20 @@ public class RG_videoMechanics : MonoBehaviour
         
     }
 
+    //loads interrogation scene to skip video
     public void skipVid(){
         SceneManager.LoadScene("Interrogation");
     }
 
+    //used to reverse the play-state of the video
     public void playPause(){
+        //when play button is visible and the video is paused, play the video and swap the visibility of the play and pause buttons
         if(GameObject.Find("Play").GetComponent<Image>().color == white && vP.isPaused){
             vP.Play();
             GameObject.Find("Play").GetComponent<Image>().color = transparent;
             GameObject.Find("Pause").GetComponent<Image>().color = white;
         }
+        //when pause button is visible and the video is played, pause the video and swap the visibility of the play and pause buttons
         else if(GameObject.Find("Pause").GetComponent<Image>().color == white && vP.isPlaying){
             vP.Pause();
             GameObject.Find("Pause").GetComponent<Image>().color = transparent;
